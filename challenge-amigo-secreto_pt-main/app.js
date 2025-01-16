02382b60376a307f7d9276c371345ff36ab6e568
Gustavo <gsdacosta.24@gmail.com>
2025-01-16 01:22:22 -0300

    ajustes aos requisitos do desafio

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,31 +1,36 @@
 let nameInput = document.getElementById("amigo");
 let amigos = [];
-let lista = document.getElementById("resultado");
+let lista = document.getElementById("listaAmigos");
+let listaResultado = document.getElementById("resultado");
 
 function adicionarAmigo() {
-  let nome = nameInput.value;
+  nameInput.value;
 
-  amigos.push(nome);
-  atualizarLista();
-  nameInput.value = "";
-  nameInput.focus();
+  if (nameInput.value == "") {
+    alert("Por favor, insira um nome.");
+  } else {
+    amigos.push(nameInput.value);
+    atualizarLista();
+    nameInput.value = "";
+    nameInput.focus();
+  }
 }
 
 function atualizarLista() {
-  let listaMetodo = lista;
-  lista.innerHTML = ""; // Limpa a lista antes de atualizá-la
+  lista.innerHTML = "";
   amigos.forEach((amigo) => {
     let li = document.createElement("li");
     li.textContent = amigo;
-    listaMetodo.appendChild(li);
+    lista.appendChild(li);
   });
 }
 
 function sortearAmigo() {
-  let listaSorteio = document.querySelectorAll(`#resultado li`);
+  let indice = Math.floor(Math.random() * amigos.length);
 
-  let indice = Math.floor(Math.random() * listaSorteio.length);
-  let itemAleatorio = listaSorteio[indice];
-
-  alert(`O amigo sorteado é ${itemAleatorio.textContent}`);
+  if (amigos.length === 0) {
+    alert("Lista vazia!");
+  } else {
+    listaResultado.innerHTML = amigos[indice];
+  }
 }
