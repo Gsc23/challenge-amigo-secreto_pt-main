f3a31d9e1fc0f320fb24890764c13040b6b9739a
Gustavo Souza <127570337+Gsc23@users.noreply.github.com>
2025-01-17 15:37:30 -0300

    Update app.js logic

--- app.js
+++ app.js
@@ -4,33 +4,29 @@ let lista = document.getElementById("listaAmigos");
 let listaResultado = document.getElementById("resultado");
 
 function adicionarAmigo() {
-  nameInput.value;
 
-  if (nameInput.value == "") {
-    alert("Por favor, insira um nome.");
-  } else {
-    amigos.push(nameInput.value);
-    atualizarLista();
-    nameInput.value = "";
-    nameInput.focus();
-  }
+  if (nameInput.value.trim() === "") {
+    return alert("Por favor, insira um nome.");
+  } 
+  
+  amigos.push(nameInput.value);
+  atualizarLista();
+  nameInput.value = "";
+  nameInput.focus();
 }
 
 function atualizarLista() {
-  lista.innerHTML = "";
-  amigos.forEach((amigo) => {
-    let li = document.createElement("li");
-    li.textContent = amigo;
-    lista.appendChild(li);
-  });
+  let listaHTML = amigos.map((amigo) => `<li>${amigo}</li>`).join('');
+  lista.innerHTML = listaHTML;
 }
 
 function sortearAmigo() {
   let indice = Math.floor(Math.random() * amigos.length);
 
   if (amigos.length === 0) {
-    alert("Lista vazia!");
-  } else {
-    listaResultado.innerHTML = amigos[indice];
+    return alert("Lista vazia!");
+  }
+  
+  listaResultado.innerHTML = amigos[indice];
   }
 }
